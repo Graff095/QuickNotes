500770197661fb5205e849a92c5712cc2eb3f723
Graff095 <100046552+Graff095@users.noreply.github.com>
2024-12-14 23:35:33 +0300

    Создана модель заметок, добавлены тестовые данные и реализовано отображение в UITableView

--- QuickNotes/MainViewController.swift
+++ QuickNotes/MainViewController.swift
@@ -10,31 +10,67 @@ import UIKit
 class MainViewController: UIViewController {
    
     private var tableView: UITableView!
+    private var notes: [Note] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView = UITableView(frame: view.bounds, style: .plain)
-        view.addSubview(tableView)
+        setupUI()
+        loadNotes()
+        tableView.reloadData()
         
-        tableView.dataSource = self
-        tableView.delegate = self
     }
 
 
+    private func  setupUI() {
+        
+        // Настраиваем навигацию
+            title = "QuickNotes"
+            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNote))
+
+        
+            // Настраиваем таблицу
+            tableView = UITableView(frame: view.bounds, style: .plain)
+            tableView.dataSource = self
+            tableView.delegate = self
+
+            // Регистрация ячейки
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+            view.addSubview(tableView)
+
+    }
+    
+    
+    private func loadNotes() {
+        notes = [
+            Note(id: UUID(), text: "Купить молоко", date: Date()),
+            Note(id: UUID(), text: "Позвонить другу", date: Date()),
+            Note(id: UUID(), text: "Прочитать книгу", date: Date())
+        ]
+    }
+    
+    
+    
+    @objc private func addNote() {
+        print("Добавить новую заметку")
+    }
+    
 }
 
 extension MainViewController:UITableViewDelegate, UITableViewDataSource{
    
     //Сколько строк в таблице:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notes.count
     }
     
     // Что показывать в каждой строке
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        cell.textLabel?.text = "\(indexPath.row + 1)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let note = notes[indexPath.row]
+        cell.textLabel?.text = note.text
         return cell
     }
     
---
+++ QuickNotes/Models/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  QuickNotes
+//
+//  Created by Visarg on 14.12.2024.
+//
+
+import Foundation
+
+struct Note {
+    let id: UUID
+    var text: String
+    let date: Date
+}
